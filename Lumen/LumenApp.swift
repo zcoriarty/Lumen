18a221ec5cfd0ea6b30a863b5064f9a15f962098
Zach Coriarty <zackcor27@gmail.com>
2023-06-24 11:09:56 -0400

    Initial Commit

---
+++ Lumen/LumenApp.swift
@@ -0,0 +1,17 @@
+//
+//  LumenApp.swift
+//  Lumen
+//
+//  Created by Zachary Coriarty on 6/24/23.
+//
+
+import SwiftUI
+
+@main
+struct LumenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
